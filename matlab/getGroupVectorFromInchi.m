82b9c4668b7f312f0a0abcab728ba0f8c73e3c0c
Elad Noor <eladn@eladn-MacBookAir.(none)>
2013-04-10 13:46:51 +0300

    initial version copied from kiln

---
+++ matlab/getGroupVectorFromInchi.m
@@ -0,0 +1,31 @@
+function group_def = getGroupVectorFromInchi(inchi, silent)
+
+if nargin < 2
+    silent = true;
+end
+
+if isempty(inchi)
+    group_def = [];
+    return;
+end
+
+if silent
+    cmd = ['python ' getBasePath() 'python/inchi2gv.py -s -i '];
+else
+    cmd = ['python ' getBasePath() 'python/inchi2gv.py -i '];
+end
+
+if ~ispc
+    [rval, group_def] = system([cmd, '"', inchi, '"']);
+else
+    [rval, group_def] = system([cmd, inchi]);
+end
+
+if rval == 0 && ~strcmp('Traceback', group_def(1:9))
+    group_def = regexp(group_def, ',', 'split');
+    group_def = group_def(~cellfun('isempty',group_def));
+    group_def = str2double(group_def);
+else
+    group_def = [];
+end
+
