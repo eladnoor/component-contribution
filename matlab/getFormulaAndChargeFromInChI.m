139aade9daea9b90aee282dee3a0d0ec2bceab80
huldash <huldash@hotmail.com>
2013-07-31 19:47:41 +0000

    Rewrote getFormulaAndChargeFromInChI.m. It now uses cxcalc to get formula and charge.

--- matlab/getFormulaAndChargeFromInChI.m
+++ matlab/getFormulaAndChargeFromInChI.m
@@ -1,57 +1,50 @@
 function [formula, nH, charge] = getFormulaAndChargeFromInChI(inchi)
-
+% Extracts chemical formula, number of hydrogen atoms and charge of a
+% metabolite from its InChI. Depends on ChemAxon's Calculator Plugins
+% (cxcalc).
+% 
 % [formula,charge] = getFormulaAndChargeFromInChI(inchi)
 % 
 % INPUT
-% inchi.......Nonstandard IUPAC InChI for a particular pseudoisomer of a
-%             metabolite
+% inchi.......IUPAC InChI for a particular pseudoisomer of a metabolite
 % 
 % OUTPUTS
 % formula....The chemical formula for the input pseudoisomer
+% nH.........The number of hydrogen atoms in formula
 % charge.....The charge on the input pseudoisomer
 
-
-layers = regexp(inchi,'/','split');
-f1 = layers{2}; % Fully protonated formula
-
-p = {};
-if ~isempty(strmatch('p',layers))
-    p = layers{strmatch('p',layers)}; % nH to add or subtract
-end
-
-q = {};
-if ~isempty(strmatch('q',layers))
-    q = layers{strmatch('q',layers)}; % charge
-end
-
-if ~isempty(q)
-    charge = str2double(q(2:end));
+[s,r] = system(['cxcalc formula "' inchi '"']);
+pat1 = 'id\tFormula\n\d+\t(?<formula>.+)';
+if s == 0 && regexp(r,pat1) == 1
+    formula = regexp(r,pat1,'names');
+    formula = formula.formula;
 else
-    charge = 0;
+    formula = '';
 end
 
-f1_nH = getNumAtomsOfElementInFormula(f1,'H'); % nH in fully protonated formula
-if ~isempty(p)
-    nH = f1_nH + str2double(p(2:end)); % nH in pseudoisomer formula
+if ~isempty(formula)
+    pat2 = '(?<u>[A-Z][a-z]*\d*)';
+    u = regexp(formula,pat2,'names');
+    u = {u.u}';
+    e = regexprep(u,'\d*','');
+    n = regexprep(u,'[A-Z_a-z]*','');
+    n(cellfun('isempty',n)) = {'1'};
+    n = str2double(n);
+    if any(strcmp(e,'H'))
+        nH = n(strcmp(e,'H'));
+    else
+        nH = 0;
+    end
 else
-    nH = f1_nH;
+    nH = nan;
 end
 
-formula = regexprep(f1,'H[a-z]*[0-9]*',''); % Remove all H from fully protonated formula
-if nH == 1
-    formula = [formula 'H'];
-elseif nH > 1
-    formula = [formula 'H' num2str(nH)]; % Add appropriate nH back in to create pseudoisomer formula
-elseif nH < 0
-    error('Negative number of H in formula.') % Should never get here
-end
-
-% In case there is Hg in formula
-f1_nHg = getNumAtomsOfElementInFormula(f1,'Hg');
-if f1_nHg == 1
-    formula = [formula 'Hg'];
-elseif f1_nHg > 1
-    formula = [formula 'Hg' num2str(f1_nHg)];
-end
-
+[s,r] = system(['cxcalc formalcharge "' inchi '"']);
+pat3 = 'id\tFormal charge\n\d+\t(?<charge>-?\d+)';
+if s == 0 && regexp(r,pat3) == 1
+    charge = regexp(r,pat3,'names');
+    charge = charge.charge;
+    charge = str2double(charge);
+else
+    charge = nan;
 end
