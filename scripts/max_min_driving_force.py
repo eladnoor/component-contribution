fixing bug in iterative MDF, where the HTML report contained wrong values for some of the reactions

scripts/max_min_driving_force.py
@@ -22,7 +22,8 @@ class ConcentrationConstraints(object):
 
 class MaxMinDrivingForce(object):
     
-    def __init__(self, model, fluxes, bounds, pH, I, T, html_writer=None):
+    def __init__(self, model, fluxes, bounds, pH, I, T, html_writer=None,
+                 cid2name=None):
         """
             model    - a KeggModel object
             fluxes   - a list of fluxes, should match the model in length
@@ -32,7 +33,8 @@ class MaxMinDrivingForce(object):
             html_writer - (optional) write a report to HTML file
         """
         self.model = model
-        self.fluxes = np.matrix(fluxes)
+        self.fluxes = fluxes
+        self.cid2name = cid2name
         self.pH, self.I, self.T = pH, I, T
         self.c_range = default_c_range
         self.c_mid = default_c_mid
@@ -50,6 +52,12 @@ class MaxMinDrivingForce(object):
                           'Default concentration = %g M' % self.c_mid]
         self.html_writer.write_ul(condition_list)
 
+    def MapCIDs(self, cid_dict):
+        try:
+            self.cids = map(cid_dict.get, self.cids)
+        except KeyError as e:
+            raise KeyError('Error: not all CIDs are mapped to their new names '
+                           + str(e))
     def GetBounds(self):
         cid2bounds = {cid:self.c_range for cid in self.model.cids}
         cid2bounds['C00001'] = (1, 1) # the default for H2O is 1
@@ -59,14 +67,15 @@ class MaxMinDrivingForce(object):
 
     def Solve(self, uncertainty_factor=3.0):
         S = self.model.S
-        rids = self.model.rids or ['R%05d' % i for i in xrange(S.shape[1])]
         cids = self.model.cids
+        rids = self.model.rids or ['R%05d' % i for i in xrange(S.shape[1])]
         dG0_prime, dG0_std = self.model.get_transformed_dG0(pH=self.pH, I=self.I, T=self.T)
         cid2bounds = self.GetBounds()
 
         keggpath = KeggPathway(S, rids, self.fluxes, cids, dG0_prime,
                                uncertainty_factor*dG0_std,
-                               cid2bounds=cid2bounds, c_range=self.c_range)
+                               cid2bounds=cid2bounds, c_range=self.c_range,
+                               cid2name=self.cid2name)
         _mdf, params = keggpath.FindMDF()
         total_dG_prime = params['maximum total dG']
         odfe = 100 * np.tanh(_mdf / (2*R*self.T))
@@ -98,39 +107,98 @@ class MaxMinDrivingForce(object):
         cid2bounds = self.GetBounds()
         
         rid2bounds = {}
-        bound_reaction_counter = 0
         total_active_reactions = len(filter(None, f.flat))
         
-        while bound_reaction_counter < total_active_reactions:
+        iter_counters = [-1] * len(rids)
+        params_list = []
+        for i in xrange(len(rids)):
             keggpath = KeggPathway(S, rids, f, cids, dG0_prime,
                                    uncertainty_factor*dG0_std,
                                    rid2bounds=rid2bounds,
                                    cid2bounds=cid2bounds,
-                                   c_range=self.c_range)
+                                   c_range=self.c_range,
+                                   cid2name=self.cid2name)
             _mdf, params = keggpath.FindMDF(calculate_totals=False)
+            params_list.append(params)
             
+            # is not the same and maybe there is a mixup
+            tmp = zip(rids,
+                      map(lambda x:'%.1f' % x, params['gibbs energies'].flat),
+                      map(lambda x:'%.1f' % x, params['reaction prices'].flat))
+            
+            logging.debug('\n'.join(map(', '.join, tmp)))
+        
             # fix the driving force of the reactions that have shadow prices
             # to the MDF value, and remove them from the optimization in the
             # next round
             shadow_prices = params['reaction prices']
             
             print '\rIterative MDF: %3d%%' % \
-                (bound_reaction_counter * 100 / total_active_reactions),
+                (len(rid2bounds) * 100 / total_active_reactions),
             for rid, s_p in zip(rids, shadow_prices):
                 if rid not in rid2bounds and s_p > 1e-5:
-                    rid2bounds[rid] = -_mdf
-                    bound_reaction_counter += 1
+                    rid2bounds[rid] = -_mdf + 1e-4 # add 'epsilon' for numerical reasons
+                    iter_counters[rids.index(rid)] = i
+
+            if len(rid2bounds) == total_active_reactions: 
+                break
         
         print '\rIterative MDF: [DONE]'
-        self.html_writer.write("<p>MDF = %.1f kJ/mol</p>\n" % _mdf)
+        self.html_writer.write("<p>MDF = %.1f kJ/mol</p>\n" % params_list[0]['MDF'])
         
-        params['profile figure'] = keggpath.PlotProfile(params)
-        self.html_writer.embed_matplotlib_figure(params['profile figure'],
+        params_list[-1]['profile figure'] = keggpath.PlotProfile(params_list[-1])
+        self.html_writer.embed_matplotlib_figure(params_list[-1]['profile figure'],
                                                  width=320, height=320)
-        keggpath.WriteResultsToHtmlTables(self.html_writer, params)
+        self.WriteIterativeReport(keggpath, params_list, iter_counters)
+        return params_list[-1]
 
-        return params
+    def WriteIterativeReport(self, keggpath, params_list, iter_counters):
+        headers = ["reaction", "formula", "flux",
+                   "&Delta;<sub>r</sub>G' [kJ/mol]"] + ['I%02d' % i for i in xrange(len(params_list))]
+        dict_list = []
+        for r, rid in enumerate(keggpath.rids):
+            if keggpath.fluxes[0, r] == 0:
+                continue
+            d = {'reaction'  : rid,
+                 'flux'      : keggpath.fluxes[0, r],
+                 'formula'   : keggpath.GetReactionString(r),
+                 headers[3]  : params_list[-1]['gibbs energies'][r, 0],
+                 'iteration' : iter_counters[r]}
+            for i, p in enumerate(params_list):
+                if i < iter_counters[r]:
+                    d['I%02d' % i] = '%.3f' % p['gibbs energies'][r, 0]
+                else:
+                    d['I%02d' % i] = '<b>%.3f</b>' % p['gibbs energies'][r, 0]
             
+            dict_list.append(d)
+
+        dict_list.sort(key=lambda d:d['iteration'], reverse=False)
+
+        d = {'reaction' : 'Total',
+             'flux'     : '1',
+             'formula'  : keggpath.GetTotalReactionString(),
+             headers[3] : float(keggpath.fluxes * params_list[-1]['gibbs energies'])}
+        dict_list.append(d)
+        
+        self.html_writer.write_table(dict_list, headers=headers, decimal=1)
+
+        concentrations = params_list[-1]['concentrations']
+
+        headers = ['compound', 'Concentration LB [M]',
+                   'Concentration [M]', 'Concentration UB [M]']
+        dict_list = []
+        for c, cid in enumerate(keggpath.cids):
+            d = {}
+            d['compound'] = keggpath.c_names[c]
+            lb, ub = keggpath.GetConcentrationBounds(cid)
+            d['Concentration LB [M]'] = '%.2e' % lb
+            d['Concentration [M]'] = '%.2e' % concentrations[c, 0]
+            d['Concentration UB [M]'] = '%.2e' % ub
+            dict_list.append(d)
+       
+        self.html_writer.write_table(dict_list, headers=headers)
+
+###############################################################################
             
 def KeggFile2ModelList(pathway_file):
     kegg_file = ParsedKeggFile.FromKeggFile(pathway_file)

scripts/mdf_dual.py
@@ -59,6 +59,7 @@ class Pathway(object):
         assert self.fluxes.shape[1] == self.Nr
         
         self.I_dir = np.matrix(np.diag(map(np.sign, self.fluxes.flat)))
+        self.Nr_active = int(sum(self.fluxes.T != 0))
         self.c_bounds = None
         self.r_bounds = None
         self.c_range = self.DEFAULT_C_RANGE
@@ -120,19 +121,37 @@ class Pathway(object):
             B is the max-min driving force variable which is being maximized
             by the LP
         """
-       
-        A = np.matrix(np.vstack([np.hstack([self.I_dir * self.dG0_r_std / default_RT, self.I_dir * self.S.T, np.ones((self.Nr, 1)) ]),
-                                 np.hstack([ np.eye(self.Nr),              np.zeros((self.Nr, self.Nc+1))]),
-                                 np.hstack([-np.eye(self.Nr),              np.zeros((self.Nr, self.Nc+1))]),
-                                 np.hstack([np.zeros((self.Nc, self.Nr)),  np.eye(self.Nc), np.zeros((self.Nc, 1))]),
-                                 np.hstack([np.zeros((self.Nc, self.Nr)), -np.eye(self.Nc), np.zeros((self.Nc, 1))])]))
-        b = np.matrix(np.vstack([-self.I_dir * self.dG0_r_prime / default_RT,
-                                 np.ones((self.Nr, 1)),
-                                 np.ones((self.Nr, 1)),
-                                  ln_conc_ub,
-                                 -ln_conc_lb]))
-        c = np.matrix(np.vstack([np.zeros((self.Nr + self.Nc, 1)),
-                                 np.ones((1, 1))]))
+        inds = np.nonzero(np.diag(self.I_dir))[0].tolist()
+        
+        # driving force
+        A11 = self.I_dir[inds] * self.dG0_r_std
+        A12 = self.I_dir[inds] * self.S.T * default_RT
+        A13 = np.ones((len(inds), 1))
+        
+        # covariance var ub and lb
+        A21 = np.eye(self.Nr)
+        A22 = np.zeros((self.Nr, self.Nc))
+        A23 = np.zeros((self.Nr, 1))
+        
+        # log conc ub and lb
+        A31 = np.zeros((self.Nc, self.Nr))
+        A32 = np.eye(self.Nc)
+        A33 = np.zeros((self.Nc, 1))
+        
+        # upper bound values
+        b1 = -self.I_dir[inds] * self.dG0_r_prime
+        b2 = np.ones((self.Nr, 1))
+        
+        A = np.matrix(np.vstack([np.hstack([ A11,  A12,  A13]),   # driving force
+                                 np.hstack([ A21,  A22,  A23]),   # covariance var ub 
+                                 np.hstack([-A21,  A22,  A23]),   # covariance var lb 
+                                 np.hstack([ A31,  A32,  A33]),   # log conc ub
+                                 np.hstack([ A31, -A32,  A33])])) # log conc lb
+
+        b = np.matrix(np.vstack([b1, b2, b2, ln_conc_ub, -ln_conc_lb]))
+
+        c = np.matrix(np.zeros((A.shape[1], 1)))
+        c[-1, 0] = 1.0
 
         # change the constaints such that reaction that have an explicit
         # r_bound will not be constrained by B, but will be constained by
@@ -142,8 +161,8 @@ class Pathway(object):
         if self.r_bounds:
             for i, r_ub in enumerate(self.r_bounds):
                 if r_ub is not None:
-                    A[i, -1] = 0
-                    b[i] += r_ub/default_RT + 1e-5 
+                    A[i, -1] = 0.0
+                    b[i, 0] += r_ub
         
         return A, b, c
    
@@ -172,9 +191,9 @@ class Pathway(object):
         A, b, c = self._MakeDrivingForceConstraints(ln_conc_lb, ln_conc_ub)
        
         w = pulp.LpVariable.dicts("w", 
-                                  ["%d" % i for i in xrange(self.Nr)],
+                                  ["%d" % i for i in xrange(self.Nr_active)],
                                   lowBound=0)
-        w = [w["%d" % i] for i in xrange(self.Nr)]
+        w = [w["%d" % i] for i in xrange(self.Nr_active)]
 
         g = pulp.LpVariable.dicts("g", 
                                   ["%d" % i for i in xrange(2*self.Nr)],
@@ -182,7 +201,7 @@ class Pathway(object):
         g = [g["%d" % i] for i in xrange(2*self.Nr)]
 
         z = pulp.LpVariable.dicts("z", 
-                                  ["%d" % i for i in xrange(2*self.Nc)],
+                                  ["%d" % i for i in xrange(self.Nc)],
                                   lowBound=0)
         z = [z["%d" % i] for i in xrange(self.Nc)]
 
@@ -231,7 +250,7 @@ class Pathway(object):
         x = y + l + [B]
         lp = pulp.LpProblem("MDF_PRIMAL", pulp.LpMaximize)
         
-        cnstr_names = ["driving_force_%02d" % j for j in xrange(self.Nr)] + \
+        cnstr_names = ["driving_force_%02d" % j for j in xrange(self.Nr_active)] + \
                       ["covariance_var_ub_%02d" % j for j in xrange(self.Nr)] + \
                       ["covariance_var_lb_%02d" % j for j in xrange(self.Nr)] + \
                       ["log_conc_ub_%02d" % j for j in xrange(self.Nc)] + \
@@ -291,14 +310,13 @@ class Pathway(object):
         lp_primal.solve(pulp.CPLEX(msg=0))
         if lp_primal.status != pulp.LpStatusOptimal:
             raise pulp.solvers.PulpSolverError("cannot solve MDF primal")
-        y = map(pulp.value, y)
-        l = map(pulp.value, l)
+        y = np.matrix(map(pulp.value, y)).T
+        l = np.matrix(map(pulp.value, l)).T
         mdf = pulp.value(B)
-        conc = np.matrix(map(np.exp, l)).T
-        dG0_r_prime = self.dG0_r_prime + np.dot(self.dG0_r_std, y).T
+        conc = np.exp(l)
+        dG0_r_prime = self.dG0_r_prime + np.dot(self.dG0_r_std, y)
 
         lp_dual, w, g, z, u = self._MakeMDFProblemDual()
-        
         lp_dual.solve(pulp.CPLEX(msg=0))
         if lp_dual.status != pulp.LpStatusOptimal:
             raise pulp.solvers.PulpSolverError("cannot solve MDF dual")
@@ -306,9 +324,10 @@ class Pathway(object):
         z = map(pulp.value, z)
         u = map(pulp.value, u)
         reaction_prices = np.matrix(w).T
+        
         compound_prices = np.matrix(z).T - np.matrix(u).T
 
-        params = {'MDF': mdf * default_RT,
+        params = {'MDF': mdf,
                   'reaction energies': dG0_r_prime,
                   'concentrations' : conc,
                   'reaction prices' : reaction_prices,
@@ -326,7 +345,7 @@ class Pathway(object):
             else:
                 min_tot_dg = pulp.value(total_dg)
     
-            params['minimum total dG'] = min_tot_dg * default_RT
+            params['minimum total dG'] = min_tot_dg
     
             lp_total, total_dg = self._GetTotalEnergyProblem(mdf - 1e-6, pulp.LpMaximize)
             lp_total.solve(pulp.CPLEX(msg=0))
@@ -337,16 +356,16 @@ class Pathway(object):
             else:
                 max_tot_dg = pulp.value(total_dg)
 
-            params['maximum total dG'] = max_tot_dg * default_RT
+            params['maximum total dG'] = max_tot_dg
             
         dG_r_prime = self.CalculateReactionEnergiesUsingConcentrations(conc)
-        params['gibbs energies raw'] = dG_r_prime + np.dot(self.dG0_r_std, y).T
+        params['gibbs energies raw'] = dG_r_prime + np.dot(self.dG0_r_std, y)
 
         # adjust dG to flux directions
         dG_r_prime_adj = self.I_dir * params['gibbs energies raw']
         params['gibbs energies'] = dG_r_prime_adj
         
-        return mdf * default_RT, params
+        return mdf, params
 
 class KeggPathway(Pathway):
    
@@ -354,7 +373,8 @@ class KeggPathway(Pathway):
                  dG0_r_prime, dG0_r_std=None,
                  rid2bounds=None,
                  cid2bounds=None,
-                 c_range=None):
+                 c_range=None,
+                 cid2name=None):
                      
         """
             S           - the stoichiometric matrix
@@ -391,6 +411,11 @@ class KeggPathway(Pathway):
         
         self.rid2bounds = rid2bounds
         self.c_range = c_range
+        
+        if cid2name is None:
+            self.c_names = self.cids
+        else:
+            self.c_names = [cid2name.get(c, c) for c in self.cids]
 
     def GetConcentrationBounds(self, cid):
         lb, ub = None, None
@@ -409,9 +434,9 @@ class KeggPathway(Pathway):
             pass
         return conc
 
-    def GetReactionString(self, r, show_cids=False):
+    def GetReactionString(self, r):
         rid = self.rids[r]
-        sparse = dict([(self.cids[c], self.S[c, r])
+        sparse = dict([(self.c_names[c], self.S[c, r])
                        for c in self.S[:, r].nonzero()[0].flat])
         if self.fluxes[0, r] >= 0:
             direction = '=>'
@@ -420,9 +445,9 @@ class KeggPathway(Pathway):
         reaction = KeggReaction(sparse, arrow=direction, rid=rid)
         return str(reaction)
 
-    def GetTotalReactionString(self, show_cids=False):
+    def GetTotalReactionString(self):
         total_S = self.S * self.fluxes.T
-        sparse = dict([(self.cids[c], total_S[c, 0])
+        sparse = dict([(self.c_names[c], total_S[c, 0])
                        for c in total_S.nonzero()[0].flat])
         reaction = KeggReaction(sparse, arrow="=>", rid="Total")
         return str(reaction)
@@ -528,7 +553,7 @@ class KeggPathway(Pathway):
             d = {}
             d['reaction'] = rid
             d['flux'] = "%g" % abs(self.fluxes[0, r])
-            d['formula'] = self.GetReactionString(r, show_cids=False)
+            d['formula'] = self.GetReactionString(r)
             d[headers[3]] = dG_r_prime_c_adj[r, 0]
             d[headers[4]] = dG_r_prime_adj[r, 0]
             d[headers[5]] = '%.3g' % reaction_shadow_prices[r, 0]
@@ -537,7 +562,7 @@ class KeggPathway(Pathway):
 
         d = {'reaction':'Total',
              'flux':'1',
-             'formula':self.GetTotalReactionString(show_cids=False),
+             'formula':self.GetTotalReactionString(),
              headers[3]: float(self.fluxes * self.dG0_r_prime),
              headers[4]: float(self.fluxes * dG_r_prime),
              headers[5]: '%.3g' % np.sum(reaction_shadow_prices[:, 0])}
@@ -556,14 +581,14 @@ class KeggPathway(Pathway):
         dict_list = []
         for c, cid in enumerate(self.cids):
             d = {}
-            d['KEGG CID'] = cid
+            d['compound'] = self.c_names[c]
             lb, ub = self.GetConcentrationBounds(cid)
             d['Concentration LB [M]'] = '%.2e' % lb
             d['Concentration [M]'] = '%.2e' % concentrations[c, 0]
             d['Concentration UB [M]'] = '%.2e' % ub
             d['shadow price'] = '%.3g' % compound_shadow_prices[c, 0]
             dict_list.append(d)
-        headers = ['KEGG CID', 'Concentration LB [M]',
+        headers = ['compound', 'Concentration LB [M]',
                    'Concentration [M]', 'Concentration UB [M]', 'shadow price']
        
         html_writer.write_table(dict_list, headers=headers)