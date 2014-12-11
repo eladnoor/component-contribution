93a9c1e3b816b3322d6dc467e7a493dfc1ccbb12
Elad Noor <elad.noor@gmail.com>
2014-12-11 12:44:14 +0100

    adding dG'm to the wolf.py example

--- examples/wolf.py
+++ examples/wolf.py
@@ -1,4 +1,5 @@
 import sys, logging, os
+import numpy as np
 
 REACTION_FNAME = 'examples/wolf_reactions.txt'
 PYTHON_BIN = 'python'
@@ -9,6 +10,8 @@ def python_main():
     logger.setLevel(logging.INFO)
     from python.component_contribution import ComponentContribution
     from python.kegg_model import KeggModel
+    from python.thermodynamic_constants import default_RT
+
 
     cc = ComponentContribution.init()
     reaction_strings = open(REACTION_FNAME, 'r').readlines()
@@ -16,10 +19,17 @@ def python_main():
 
     model.add_thermo(cc)
     dG0_prime, dG0_std = model.get_transformed_dG0(7.0, 0.2, 298.15)
+    
+    mM_conc = 1e-3 * np.matrix(np.ones((len(model.cids), 1)))
+    if 'C00001' in model.cids:
+        mM_conc[model.cids.index('C00001'), 0] = 1.0
+    dGm_prime = dG0_prime + default_RT * model.S.T * np.log(mM_conc)
+    
     for i, r in enumerate(reaction_strings):
         print '-'*50
         print r.strip()
         print "dG0 = %8.1f +- %5.1f" % (dG0_prime[i, 0], dG0_std[i, i] * 1.96)
+        print "dGm = %8.1f +- %5.1f" % (dGm_prime[i, 0], dG0_std[i, i] * 1.96)
 
 if __name__ == '__main__':
     pwd = os.path.realpath(os.path.curdir)
