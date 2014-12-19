61d965103c1f4b96726ca99df89d743ae3068c63
Elad Noor <elad.noor@gmail.com>
2014-12-19 14:06:33 +0100

    adding a simplified chemical structure to thioredoxin

--- python/recache_compound.py
+++ python/recache_compound.py
@@ -9,6 +9,7 @@ from python.compound_cacher import CompoundCacher
 
 compound_id = sys.argv[1]
 ccache = CompoundCacher()
+CompoundCacher.RebuildCompoundJSON()
 ccache.remove(compound_id)
 comp = ccache.get_compound(compound_id)
 ccache.dump()
