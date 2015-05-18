d7a6b5b1120f262e1f1ec22891413d8e8736eaab
Elad Noor <elad.noor@gmail.com>
2015-05-18 11:28:07 +0200

    fixing alpha-D-glutamyl-P formula

--- component_contribution/recache_compound.py
+++ component_contribution/recache_compound.py
@@ -10,6 +10,12 @@ from compound_cacher import CompoundCacher
 compound_id = sys.argv[1]
 CompoundCacher.RebuildCompoundJSON()
 ccache = CompoundCacher()
+sys.stderr.write('removing %s from cache ...\n' % compound_id)
 ccache.remove(compound_id)
+sys.stderr.write('recalculating SMILES and pKa values ...\n')
 comp = ccache.get_compound(compound_id)
+sys.stderr.write('writing new data to cache ...\n')
 ccache.dump()
+
+d = comp.to_json_dict()
+sys.stderr.write(''.join(['%20s : %s\n' % (k,v) for (k,v) in d.iteritems()]))
