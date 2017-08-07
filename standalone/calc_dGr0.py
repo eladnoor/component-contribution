7003f17c7624d7bd9b5e37b6349050a6caa8d035
Avi Flamholz <flamholz@gmail.com>
2017-08-07 11:27:08 -0700

    Minor updates to help text and printing.

--- standalone/calc_dGr0.py
+++ standalone/calc_dGr0.py
@@ -15,9 +15,15 @@ import logging
 from preprocessing import Preprocessing, Reaction
 
 def MakeParser():
-    parser = argparse.ArgumentParser(description='Estimate the Gibbs energy of a reaction')
+    parser = argparse.ArgumentParser(
+        description=('Estimate the Gibbs energy of a reaction. For example,'
+                     'the following calculates dGr0 for ATP hydrolysis '
+                     'at pH 6: calc_dGr0.py --ph 6 "C00002 + C00001 = '
+                     'C00008 + C00009"'))
     parser.add_argument('--ph', type=float, help='pH level', default=7.0)
-    parser.add_argument('--i', type=float, help='ionic strength in M', default=0.1)
+    parser.add_argument('--i', type=float,
+                        help='ionic strength in M',
+                        default=0.1)
     parser.add_argument('reaction', type=str, help='reaction in KEGG notation')
     return parser
 
@@ -28,9 +34,9 @@ args = parser.parse_args()
 
 logging.getLogger().setLevel(logging.WARNING)
 
-logging.info('pH = %.1f' % args.ph)
-logging.info('I = %.1f M' % args.i)
-logging.info('Reaction = ' + args.reaction)
+print 'pH: %.1f' % args.ph
+print 'I: %.1f M' % args.i
+print 'Reaction: ' + args.reaction
 
 # parse the reaction
 reaction = Reaction.parse_formula(args.reaction)
@@ -39,6 +45,4 @@ p = Preprocessing()
 
 # use the preprocessing class to calculate the estimated dG0 and uncertainty
 dG0_prime, U = p.dG0_prime(reaction, pH=args.ph, I=args.i)
-print dG0_prime, U
-
-# transform the dG0 at the select pH and I
+print u'dGr0 = %.2f \u00B1 %.2f kJ/mol' % (dG0_prime[0, 0], U[0, 0])
