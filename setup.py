75afa0eff0a17c38e4528453d7a34aff7c9b0a82
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2018-11-18 14:54:10 +0100

    chore: fix data distribution

--- setup.py
+++ setup.py
@@ -4,17 +4,9 @@
 import versioneer
 from setuptools import setup
 
-mydata_files = ['TECRDB.csv',
-                'formation_energies_transformed.csv',
-                'redox.csv']
-data_files = [('/train/tecrdb', ['component_contribution/data/TECRDB.csv']),
-              ('/train/formation', ['component_contribution/data/formation_energies_transformed.csv']),
-              ('/train/redox', ['component_contribution/data/redox.csv']),
-              ('/train/toy', ['component_contribution/data/toy_training_data.csv'])]
 
 # Most arguments are set in the `setup.cfg`.
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    data_files=data_files
 )
